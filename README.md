cba9580f33cb45f06832f5c4e917e7d263238271
Davidf97 <davifm97@gmail.com>
2024-06-06 18:58:43 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sprint5DavidFranco
-Repository for sprint 5 of my project
+En este proyecto aprendí a tener un link entre varios programas con el fin de poder actualizar este proyecto de forma sencilla. (a traves de github, anaconda, VS code, render, entornos visuales)
+El objetivo meramente es poder crear una app a traves de un navegador, permitiendo la visualización interactiva de los datos de las ventas de coches y que este en la nube para acceso público
+- Se limpiaron los datos y se tuvo un analisis exploratorio de datos  (EDA)
+- Se creo un archivo 'app.py' para desarrollar  una aplicación web a traves de 'streamlit'
+- Despliegue de la aplicación  localmente para verificar su funcionamiento antes del despliegue público. 
---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd 
+import plotly_express as px
+
+car_data= pd.read_csv('C:/Users/David/Documents/sprint 5/sprint5DavidFranco/vehicles_us.csv')
+hist_button= st.button ('Construir histograma')
+disp_button= st.button ('Construir diagrama de dispersión')
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    fig= px.histogram(car_data, x='odometer')
+
+    st.plotly_chart(fig, use_container_width=True) #falta agregar otro botton para dispersion
+
+
+elif disp_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches') 
+    fig2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig2, use_container_width=True) 
+
